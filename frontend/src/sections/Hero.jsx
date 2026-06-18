76da7eade1a1e4b9369b0beb5ef36ee7946e5fa0
Mohamed Gougriz <mohmadgougriz@gmail.com>
2026-06-18 17:08:58 +0100

    add btn and subtitle to hero section

--- frontend/src/sections/Hero.jsx
+++ frontend/src/sections/Hero.jsx
@@ -16,6 +16,15 @@ function Hero() {
                 <h1>PERFECT SNOW</h1>
                 <h1>ESCAPE</h1>
             </div>
+            <div className="hero-subtitle">
+              <p>Snow meets style and every moment</p>
+              <p>sparkles with adventure</p>
+            </div>
+            <div className="hero-button">
+                <a href="#" className="hero-button-link">Get Passes</a>
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#1da0fa" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right-icon lucide-arrow-up-right"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg>
+            </div>
+
     </div>
     </section>
     </>
