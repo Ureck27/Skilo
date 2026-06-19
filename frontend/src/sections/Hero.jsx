fbc7cf391147731dbabc41f603890e499002cfbc
Mohamed Gougriz <mohmadgougriz@gmail.com>
2026-06-19 16:57:45 +0100

    change style and image background

--- frontend/src/sections/Hero.jsx
+++ frontend/src/sections/Hero.jsx
@@ -24,6 +24,14 @@ function Hero() {
                 <a href="#" className="hero-button-link">Get Passes</a>
                 <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="#1da0fa" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right-icon lucide-arrow-up-right"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg>
             </div>
+            <div className="icons-persons">
+              
+            </div>
+            <div className="text-below">
+              <p>RIDE WITH PASSION, LAUGH</p>
+              <p>WITH FRIENDS AND LIVE THE</p>
+              <p>MAGIC OF WINTER</p>
+            </div>
 
     </div>
     </section>
